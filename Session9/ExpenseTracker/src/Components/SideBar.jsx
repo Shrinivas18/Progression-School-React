30b23a85ad1e8acf38d2e0e16f93c683df1762fc
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2025-06-23 20:06:03 +0530

    Update SideBar.jsx

--- Session9/ExpenseTracker/src/Components/SideBar.jsx
+++ Session9/ExpenseTracker/src/Components/SideBar.jsx
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
 import menu from "../assets/menu.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function SideBar() {
   const [showSideBar, setShowSideBar] = useState(false);
+
+  const getLinkClasses = ({ isActive }) =>
+    `shadow-lg p-2 rounded-md transition-colors ${
+      isActive ? "bg-blue-500 text-white" : "text-gray-800 hover:bg-gray-200"
+    }`;
+  
   return (
     <div
-      className={showSideBar ? "w-[20%] p-3 shadow-lg" : "w-[5%] p-3 shadow-lg"}
+      className={
+        showSideBar ? "w-[20%] p-3 shadow-lg" : "w-[7%] p-3 shadow-lg"
+      }
     >
       <div>
+        {showSideBar ? (
+          <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-cyan-500 to-teal-400 text-center mb-6">
+            Expense Tracker
+          </h1>
+        ) : (
+          <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-cyan-500 to-teal-400 text-center mb-6">
+            Expense Tracker
+          </h1>
+        )}
         <img
-          className="size-4 cursor-pointer"
+          className="size-6 cursor-pointer"
           title="Menu"
           onClick={() => setShowSideBar(!showSideBar)}
           src={menu}
           alt="Menu Icon"
         />
       </div>
-      {/* 
-      ${
-                isActive
-                  ? "bg-blue-500 text-white"
-                  : "text-gray-800 hover:bg-gray-200"
-              } */}
-
       {showSideBar && (
         <div>
-          <nav className="flex flex-col mt-[10%] gap-4 ">
-            <NavLink className={`shadow-lg `} to="/">
+          <nav className="flex flex-col mt-[10%] gap-7 text-xl ">
+            <NavLink className={getLinkClasses} to="/">
               Budget
             </NavLink>
-            <NavLink className="shadow-lg" to="/expenses">
+            <NavLink className={getLinkClasses} to="/expenses">
               Expenses
             </NavLink>
-            <NavLink className="shadow-lg" to="/menu3">
+            <NavLink className={getLinkClasses} to="/menu3">
               menu 3
             </NavLink>
-            <NavLink className="shadow-lg" to="/menu4">
+            <NavLink className={getLinkClasses} to="/menu4">
               menu 4
             </NavLink>
           </nav>
