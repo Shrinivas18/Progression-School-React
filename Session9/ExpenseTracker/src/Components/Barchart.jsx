3102dec560835c29c1572461af15305f48ff9846
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2025-06-24 21:32:16 +0530

    Update Barchart.jsx

--- Session9/ExpenseTracker/src/Components/Barchart.jsx
+++ Session9/ExpenseTracker/src/Components/Barchart.jsx
@@ -31,19 +31,46 @@ function Barchart() {
     return acc;
   }, []);
 
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload;
+      return (
+        <div className="bg-white border border-gray-300 rounded shadow-md p-2 text-sm">
+          <p>{data.category.toUpperCase()}: ₹ {data.amount}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const CustomLegend = () => {
+    return (
+      <ul className="flex flex-wrap gap-4 justify-center mt-4 text-sm">
+        {groupedData.map((entry, index) => (
+          <li key={index} className="flex items-center gap-2">
+            <span
+              className="inline-block w-4 h-4 rounded"
+              style={{ backgroundColor: COLORS[index % COLORS.length] }}
+            ></span>
+            <span className="text-gray-800">{entry.category}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  
+
   return (
     <div className="p-2 mt-5">
       {groupedData.length > 0 ? (
-        <ResponsiveContainer width="100%" height={300}>
+        <ResponsiveContainer width="100%" height={400}>
           <ReBarChart
             data={groupedData}
             margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis />
             <YAxis />
-            <Tooltip />
-            <Legend />
+            <Tooltip content={CustomTooltip}/>
             <Bar dataKey="amount">
               {groupedData.map((entry, index) => (
                 <Cell
@@ -52,6 +79,7 @@ function Barchart() {
                 />
               ))}
             </Bar>
+            <Legend content={CustomLegend} />
           </ReBarChart>
         </ResponsiveContainer>
       ) : (
