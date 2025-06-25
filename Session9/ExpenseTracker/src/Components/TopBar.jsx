096847f3c45e652dc3994e72e8319d50180341da
Shrinivas Jamvant <119414094+Shrinivas18@users.noreply.github.com>
2025-06-25 19:58:51 +0530

    Update TopBar.jsx

--- Session9/ExpenseTracker/src/Components/TopBar.jsx
+++ Session9/ExpenseTracker/src/Components/TopBar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import expenseTracker from "../assets/expense-tracker.png";
 import userProfile from "../assets/user.png";
+
 function TopBar() {
   return (
-    <div className="flex gap-[75%] lg:gap-[80%] shadow-md">
+    <div className="flex justify-between items-center px-4 py-2 shadow-md w-full">
       <img
-        className="lg:size-[15%] size-[20%] p-1 mb-1"
+        className="w-28 md:w-36 lg:w-44 max-w-[60%] h-auto"
         src={expenseTracker}
         alt="Expense Tracker Logo"
       />
       <img
-        className="md:size-[2.5%] max-md:size-[3%] max-md:mt-2.5 max-sm:mt-2 max-sm:size-[3.5%]  align-middle justify-center mt-3"
+        className="w-8 h-8 md:w-9 md:h-9 object-cover rounded-full border border-gray-300"
         src={userProfile}
         alt="User Profile"
       />
